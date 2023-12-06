e9c66759530f272eadaa41a8853d805d5ebbb743
Steven Pickering <pickman510@gmail.com>
2023-12-06 15:47:40 -0500

    In JdbcBreweryDao: fixed mapRowToBrewery to pull and convert Timestamp from SQL to LocalDateTime in Java and handle null, Completed methods getAllBreweries and getSavedBreweries, Fixed zipcode in SQL statements to zip_code

--- capstone/java/src/main/java/com/techelevator/dao/JdbcBreweryDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/JdbcBreweryDao.java
@@ -4,6 +4,7 @@ import com.techelevator.exception.DaoException;
 import com.techelevator.model.Beer;
 import com.techelevator.model.BrewSearchDTO;
 import com.techelevator.model.Brewery;
+import com.techelevator.model.User;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -13,6 +14,7 @@ import java.security.Principal;
 import java.security.Timestamp;
 import java.sql.Time;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,12 +23,14 @@ public class JdbcBreweryDao implements BreweryDao {
 
     // Instance Variables/Properties
     private JdbcTemplate jdbcTemplate;
+    private JdbcUserDao jdbcUserDao;
 
 
 
     // Constructors
     public JdbcBreweryDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.jdbcUserDao = jdbcUserDao;
     }
 
 
@@ -36,7 +40,7 @@ public class JdbcBreweryDao implements BreweryDao {
     public Brewery getBreweryById (int breweryId) {
         Brewery brewery = null;
         brewery.setBreweryId(breweryId);
-        String sql = "SELECT brewery_id, brewery_name, street_address, city, state, zipcode," +
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, state, zip_code," +
                      " date_est, phone_number, about_us, website, logo_image, founder_id " +
                 "FROM brewery WHERE brewery_id = ?;";
         try {
@@ -52,7 +56,20 @@ public class JdbcBreweryDao implements BreweryDao {
 
     @Override
     public List<Brewery> getAllBreweries() {
-        return null;
+        List<Brewery> breweries = new ArrayList<>();
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, state, zip_code," +
+                " date_est, phone_number, about_us, website, logo_image, founder_id " +
+                "FROM brewery;";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            while (results.next()) {
+                Brewery brewery = mapRowToBrewery(results);
+                breweries.add(brewery);
+            }
+            return breweries;
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database.", e);
+        }
     }
 
     @Override
@@ -62,7 +79,18 @@ public class JdbcBreweryDao implements BreweryDao {
 
     @Override
     public List<Brewery> getSavedBreweries(Principal principal) {
-        return null;
+        List<Brewery> myBreweries = new ArrayList<>();
+        String sql = "SELECT brewery_id FROM favorite_brewery WHERE user_id = ?;";
+        try {
+            User user = jdbcUserDao.getUserByUsername(principal.getName());
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, user.getId());
+            while (results.next()) {
+                myBreweries.add(getBreweryById(results.getInt("brewery_id")));
+            }
+            return myBreweries;
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database.", e);
+        }
     }
 
     @Override
@@ -82,22 +110,23 @@ public class JdbcBreweryDao implements BreweryDao {
 
     // Helper Methods
     private Brewery mapRowToBrewery (SqlRowSet rs) {
-        Brewery brewery = new Brewery(
-                rs.getInt("brewery_id"),
-                rs.getString("brewery_name"),
-                rs.getString("street_address"),
-                rs.getString("city"),
-                rs.getString("state"),
-                rs.getString("zipcode"),
-                //Below getObject call/conversion from:
-                //https://stackoverflow.com/questions/51632735/jdbc-result-set-retrieve-localdatetime
-                rs.getObject("date_est", LocalDateTime.class),
-                rs.getString("phone_number"),
-                rs.getString("about_us"),
-                rs.getString("website"),
-                rs.getString("logo_image"),
-                rs.getInt("founder_id")
-        );
+        Brewery brewery = new Brewery();
+        brewery.setBreweryId(rs.getInt("brewery_id"));
+        brewery.setBreweryName(rs.getString("brewery_name"));
+        brewery.setStreetAddress(rs.getString("street_address"));
+        brewery.setCity(rs.getString("city"));
+        brewery.setState(rs.getString("state"));
+        brewery.setZipcode(rs.getString("zip_code"));
+
+        if (rs.getTimestamp("date_est") != null) {
+        brewery.setDateEst(rs.getTimestamp("date_est").toLocalDateTime());
+        }
+
+        brewery.setPhoneNumber(rs.getString("phone_number"));
+        brewery.setAboutUs(rs.getString("about_us"));
+        brewery.setWebsite(rs.getString("website"));
+        brewery.setLogoImage(rs.getString("logo_image"));
+        brewery.setFounderId(rs.getInt("founder_id"));
         return brewery;
     }
 
