33e22a2e08fe822ebfb0adb5265b58ad772c9093
Jbracken94 <jbracken94@gmail.com>
2023-12-11 20:52:51 -0500

    Continued form work

--- capstone/vue/src/components/BreweryCard.vue
+++ capstone/vue/src/components/BreweryCard.vue
@@ -55,5 +55,5 @@ export default {
   }
 img {
     height: 75px;
-}
+    }
 </style>
--- capstone/vue/src/components/CreateBreweryForm.vue
+++ capstone/vue/src/components/CreateBreweryForm.vue
@@ -1,55 +1,59 @@
 <template>
     <form class="create-brewery-form" v-on:submit.prevent="createBrewery()">
         <label for="breweryName" class="form-label">Brewery Name</label>
-        <input class="form-control" type="text" placeholder="Enter Brewery Name" v-model="newBrewery.breweryName" >
+        <input class="form-control" type="text" placeholder="Enter Brewery Name" v-model="newBrewery.breweryName">
 
         <label for="streetAddress" class="form-label">Street Address</label>
-        <input class="form-control" type="text" placeholder="Enter Street Address" v-model="newBrewery.streetAddress" >
+        <input class="form-control" type="text" placeholder="Enter Street Address" v-model="newBrewery.streetAddress">
 
         <label for="city" class="form-label">City</label>
-        <input class="form-control" type="text" placeholder="Enter City" v-model="newBrewery.city" >
+        <input class="form-control" type="text" placeholder="Enter City" v-model="newBrewery.city">
 
         <label for="state" class="form-label">State</label>
-        <input class="form-control" type="text" placeholder="Enter State, example 'PA'" v-model="newBrewery.state" >
+        <input class="form-control" type="text" placeholder="Enter State, example 'PA'" v-model="newBrewery.state">
 
         <label for="zipCode" class="form-label">ZipCode</label>
-        <input class="form-control" type="text" placeholder="Enter ZipCode" v-model="newBrewery.zipcode" >
+        <input class="form-control" type="text" placeholder="Enter ZipCode" v-model="newBrewery.zipcode">
 
         <label for="dateEst" class="form-label">Date Established</label>
-        <input class="form-control" type="text" placeholder="Enter Date Established, example 'YYYY-MM-DD'" v-model="newBrewery.dateEst" >
+        <input class="form-control" type="text" placeholder="Enter Date Established, example 'YYYY-MM-DD'"
+            v-model="newBrewery.dateEst">
 
         <label for="phoneNumber" class="form-label">Phone Number</label>
-        <input class="form-control" type="tel" placeholder="Enter Phone Number, example '(555) 555-5555'" v-model="newBrewery.phoneNumber" >
+        <input class="form-control" type="tel" placeholder="Enter Phone Number, example '(555) 555-5555'"
+            v-model="newBrewery.phoneNumber">
 
         <label for="aboutUs" class="form-label">About Us</label>
-        <textarea class="form-control" rows="5" v-model="newBrewery.aboutUs" ></textarea>
+        <textarea class="form-control" rows="5" v-model="newBrewery.aboutUs"></textarea>
 
         <label for="website" class="form-label">Website URL</label>
-        <input class="form-control" type="text" placeholder="Enter Web URL, example 'https://yourwesbiturl.com/'" v-model="newBrewery.website" >
+        <input class="form-control" type="text" placeholder="Enter Web URL, example 'https://yourwesbiturl.com/'"
+            v-model="newBrewery.website">
 
         <label for="logoImage" class="form-label">Logo Image</label>
-        <input class="form-control" type="text" placeholder="Enter Logo Image, example 'yourlogo.jpg'" v-model="newBrewery.logoImage" >
+        <input class="form-control" type="text" placeholder="Enter Logo Image, example 'yourlogo.jpg'"
+            v-model="newBrewery.logoImage">
 
-      <button type="submit" class="btn btn-primary">Submit Changes</button>
+        <button type="submit" class="btn btn-primary">Submit Changes</button>
     </form>
 </template>
   
 <script>
 
-    import BreweryService from '../services/BreweryService'
+import BreweryService from '../services/BreweryService'
 
-    export default {
-        data() {
-            return {
-                newBrewery: {}
-            }
-        },
-    
-        methods: {
+export default {
+    data() {
+        return {
+            newBrewery: {}
+        }
+    },
 
-            createBrewery() {
-                BreweryService.createBrewery(this.newBrewery)
-                .then (response => {
+    methods: {
+
+        createBrewery() {
+            BreweryService.createBrewery(this.newBrewery)
+                .then(response => {
                     if (response.status == 200) {
                         //Notification of successful
                         alert('Success!');
@@ -61,12 +65,10 @@
                         alert(response.status);
                     }
                 });
-                // Look at setNotification from Lecture
-            }
+            // Look at setNotification from Lecture
         }
     }
+}
 </script>
   
-<style>
-  
-</style>
+<style></style>
